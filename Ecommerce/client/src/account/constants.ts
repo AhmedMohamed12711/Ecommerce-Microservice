fd2f6462323e6b41a4da81862dcee01f3899da97
Ahmed Mohamed <ahmedmohamed00521@gmail.com>
2026-08-23 03:49:19 +0200

    Update tunnel URL and set dynamic clientRoot for authentication

--- Ecommerce/client/src/account/constants.ts
+++ Ecommerce/client/src/account/constants.ts
@@ -1,6 +1,8 @@
 export class Constants {
   public static apiRoot = "https://localhost:9000";
-  public static clientRoot = "http://localhost:4200";
-  public static idpAuthority = "http://localhost:9011"
+  public static get clientRoot(): string {
+    return typeof window !== 'undefined' ? window.location.origin : "http://localhost:4200";
+  }
+  public static idpAuthority = "http://localhost:9011";
   public static clientId = "angular-client";
 }
--- Ecommerce/client/src/store/store.service.ts
+++ Ecommerce/client/src/store/store.service.ts
@@ -8,7 +8,7 @@ import { StoreParam } from '../shared/models/storeParams';
   providedIn: 'root',
 })
 export class StoreService {
-  baseUrl:string="https://cute-adults-act.loca.lt/api/v1/";
+  baseUrl:string="https://five-taxis-give.loca.lt/api/v1/";
   constructor(private http: HttpClient) {}
 
   getAllProducts(storeParam: StoreParam) {
